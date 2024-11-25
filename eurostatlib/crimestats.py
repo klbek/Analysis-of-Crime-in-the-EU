c9fa2776f5702c157f010d01af683c358798d6b8
KlaraBek <sadecka.kl@gmail.com>
2024-11-25 15:13:37 +0100

    kompletni restrukturalizace kodu, nazvu do slozek i v ramci OOP

---
+++ eurostatlib/crimestats.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy
+from dataclasses import dataclass
+
+@dataclass
+class Statistics:
+    count_years: int = pd.NA
+    count_fill_values: int = pd.NA
+    first_fill_year: int = pd.NA
+    last_fill_year: int = pd.NA
+    mean_value: float = numpy.nan
+    median_value: float = numpy.nan
+    max_value: float = numpy.nan
+    max_value_year: int = pd.NA
+    min_value: float = numpy.nan
+    min_value_year: int = pd.NA
+    standard_deviation: float = numpy.nan
+    quality_range_fill_data: int = pd.NA
+    quality_range_unfill_data: int = pd.NA
+    missing_values_info: str = pd.NA
+    trend: str = pd.NA
+    relative_trend_strength: float = numpy.nan
+    min_range_year: int = pd.NA
+    max_range_year: int = pd.NA # otázka zda používat, je to informace pro range widget
+    statistics_dictionary: dict = None
+    
+    def get_statistics_dict_info(self):
+        self.statistics_dictionary = {
+            'count_years': self.count_years,
+            'count_fill_values': self.count_fill_values,            
+            'first_fill_year': self.first_fill_year,
+            'last_fill_year': self.last_fill_year,
+            'mean_value': self.mean_value,
+            'median_value': self.median_value,
+            'max_value': self.max_value,
+            'max_value_year': self.max_value_year,
+            'min_value': self.min_value,
+            'min_value_year': self.min_value_year,
+            'standard_deviation': self.standard_deviation,
+            'quality_range_fill_data': self.quality_range_fill_data,
+            'quality_range_unfill_data': self.quality_range_unfill_data,
+            'missing_values_info': self.missing_values_info,
+            'trend': self.trend, 
+            'relative_trend_strength': self.relative_trend_strength,
+            'min_range_year': self.min_range_year,
+            'max_range_year': self.max_range_year
+        }
+
+    def _check_trend(self, data):
+        if self.quality_range_unfill_data != 0:
+            self.trend = 'to missing value(s)'
+        else:
+            diffs = numpy.diff(data)
+            sum_positive_number = numpy.sum(diffs[diffs > 0]) 
+            sum_negative_number = numpy.sum(diffs[diffs < 0])
+
+            # Pokud je soucet kladnych rozdilu vetsi nez soucet absolutnich hodnot zapornych rozdilu
+            if sum_positive_number > abs(sum_negative_number):
+                self.trend = 'increasing'
+                self.relative_trend_strength  = round(sum_positive_number / (sum_positive_number + abs(sum_negative_number)), 2)
+
+            
+            elif abs(sum_negative_number) > sum_positive_number:
+                self.trend = 'decreasing'
+                self.relative_trend_strength  = round(abs(sum_negative_number) / (sum_positive_number + abs(sum_negative_number)), 2)
+            
+        #TODO: možno upravit fci tak, aby u ojediněle chybějících hodnot doplnila pomocí lineární interpolace hodnoty.
+
+
+    def _detect_missing_values(self, data = None, idx_first_fill_year = None, idx_last_fill_year = None):
+        if self.count_fill_values == 0:
+            self.missing_values_info = f'No record for crime.'
+
+        elif self.count_fill_values == 1:
+            self.missing_values_info = f'Only 1 record for crime.'
+           
+        else:
+            count_fill_values_range = data[idx_first_fill_year:(idx_last_fill_year + 1)]['value'].count()
+            count_fill_year_range = data[idx_first_fill_year:(idx_last_fill_year + 1)]['year'].count()
+            self.quality_range_fill_data = count_fill_year_range
+            self.quality_range_unfill_data = count_fill_year_range - count_fill_values_range
+
+            if count_fill_values_range == count_fill_year_range:
+                self.missing_values_info = 'The time series has no missing values within the data disclosure period.'
+            else:
+                number = count_fill_year_range - count_fill_values_range
+                self.missing_values_info = f'The time series has {number} missing value(s) within the data disclosure period.'
+            self._check_trend(data['value'])
+
+            
+
+    def _calculate_from_data(self, data, code):
+        if code != '1-1': # prozatimni ponechani, aby si clovek mohl filtroval tabulku, jak potrebuje
+            self.missing_values_info = f'No statistics for this filtered data.'
+
+        else:
+            value_column = data['value']
+            year_column = data['year']
+
+            self.count_years = year_column.count()
+            self.count_fill_values = value_column.count()
+
+             # TODO: nutno pořešit v rámci změny filtrování, např. pokud budu mít všechny země, tento údaj úplně nědává smysl, 
+            self.min_range_year = year_column.min() 
+            self.max_range_year = year_column.max()
+
+            if value_column.count() > 1:
+                idx_first_fill_year = value_column.first_valid_index()
+                self.first_fill_year = year_column[idx_first_fill_year]
+
+                idx_last_fill_year = value_column.last_valid_index()
+                self.last_fill_year = year_column[idx_last_fill_year]
+               
+                self.mean_value = round(value_column.mean(), 2)
+                self.median_value = value_column.median()
+
+                max_value_row = data.loc[data['value'].idxmax()]
+                min_value_row = data.loc[data['value'].idxmin()]
+
+                self.max_value, self.max_value_year = max_value_row[['value', 'year']]
+                self.min_value, self.min_value_year = min_value_row[['value', 'year']]
+
+                # ddof=1 ->nastavení na výběrovou směrodatnou odchylku, bez toho populační směrodatná odchylka
+                self.standard_deviation = round(numpy.std(value_column, ddof=1), 2)
+                
+                self._detect_missing_values(data, idx_first_fill_year, idx_last_fill_year)
+                self.get_statistics_dict_info()
+
+            elif value_column.count() == 1:
+                idx_first_fill_year = value_column.first_valid_index()
+                self.min_value = data.loc[idx_first_fill_year, 'value']
+                self.min_value_year = data.loc[idx_first_fill_year, 'year']
+                self._detect_missing_values(data)
+                self.get_statistics_dict_info()
+                
+            else:
+                self._detect_missing_values(data)
+                self.get_statistics_dict_info()
---
+++ eurostatlib/crimetable.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy
+from dataclasses import dataclass
+from eurostatlib.crimestats import Statistics
+
+@dataclass
+class EurostatCrimeTable:
+    data: pd.DataFrame = None
+    country_list_sorted: list = None
+    crime_list_sorted: list = None
+    country: str = None
+    crime: str = None
+    filtered_data: pd.DataFrame = None
+    filtered_code: None = None
+    statistics: Statistics = None
+    statistics_info: str = None 
+    
+
+    def _get_sorted_list(self, unpivot_data):
+        country_list_sorted = sorted(unpivot_data['country_name'].unique())
+        country_list_sorted.insert(0, 'all')
+        self.country_list_sorted = country_list_sorted
+
+        crime_list_sorted = sorted(unpivot_data['crime_info'].unique())
+        crime_list_sorted.insert(0, 'all')
+        self.crime_list_sorted = crime_list_sorted
+
+        # TODO filtrace crime dle toho, jestli ma dana zeme u crime alespon 1 vyplnenou hodnotu
+
+    def load_data(self, path, geo_df, iccs_df):
+        data = pd.read_csv(path, sep='\t')
+
+        data = data.rename({data.columns[0]: data.columns[0].replace(
+            r"\TIME_PERIOD", "")}, axis="columns")
+        split_name_columns = (data.columns[0]).split(',')
+        count_split_columns = len(split_name_columns)
+        data[split_name_columns] = data[data.columns[0]
+                                        ].str.split(',', expand=True)
+        # odstraneni jiz rozdeleneho sloupce
+        data = data.drop(data.columns[0], axis="columns")
+
+        data = data.merge(geo_df, how='left', on='geo')
+        data = data.merge(iccs_df, how='left', on='iccs')
+
+        data = data.drop(columns=['geo', 'iccs'])
+
+        # presun rozdelenych a naparovaných udaju  na zacatek df
+        reorg_columns = data.columns.tolist()
+        final_reorg_columns = reorg_columns[-(count_split_columns + 1):] + \
+            reorg_columns[:-(count_split_columns + 1)]
+        data = data[final_reorg_columns]
+
+        # podminka, ze se nejedna o rocni data
+        no_anual = data[data['freq'] != 'A'].index
+        data = data.drop(index=no_anual)
+
+        # podminka, ze se nejedna o udaj na 100tis obyvatel
+        no_p_hthab = data[data['unit'] != 'P_HTHAB'].index
+        data = data.drop(index=no_p_hthab)
+
+        years_list = data.columns[(count_split_columns + 1):]
+        info_list = data.columns[:(count_split_columns + 1)]
+
+        unpivot_data = pd.melt(
+            data, id_vars=info_list, value_vars=years_list, var_name='year', value_name='value')
+      
+        unpivot_data['year'] = unpivot_data['year'].astype('int')
+        unpivot_data['value'] = unpivot_data['value'].replace(
+            [': ', '0 ', '0.00 ', 0], numpy.nan).astype('float')
+
+        self._get_sorted_list(unpivot_data)
+        self.data = unpivot_data
+
+    def _calculate_statistics(self):
+        data = self.filtered_data
+        code = self.filtered_code
+
+        stat = Statistics()
+        stat._calculate_from_data(data, code)
+        # TODO zaktivnit calc_func
+        # self.calc_func(data, code)
+        self.statistics = stat
+
+        # overeni vyplnebych hodnot - existence range
+        if pd.notna(stat.last_fill_year) and pd.notna(stat.max_range_year):
+            if stat.last_fill_year != stat.max_range_year:
+                info = f'Data disclosure by the country began in {
+                    stat.first_fill_year} and ended in {stat.last_fill_year}.'
+            elif stat.last_fill_year == stat.max_range_year:
+                info = f'Data disclosure by the country began in {
+                    stat.first_fill_year} and continues until {stat.max_range_year}.'
+        else:
+            info = 'Data disclosure information is incomplete or missing.'
+
+        if stat.count_fill_values in [0, 1]:
+            self.statistics_info = f'During a {stat.count_years}-year period, {self.country} recorded {
+                stat.count_fill_values} entries for {self.crime} types of crime. {info}'
+        elif stat.count_fill_values not in [0, 1]:
+            self.statistics_info = f'During a {stat.count_years}-year period, {self.country} recorded {stat.count_fill_values} entries for {self.crime} types of crime. {info} Across these years, there were an average of {
+                stat.mean_value} crimes per hundred thousand inhabitants each year and standard deviation was {stat.standard_deviation}. The minimum recorded crime rate per hundred thousand inhabitants was {stat.min_value} in {stat.min_value_year}, while the maximum was {stat.max_value} in {stat.max_value_year}.'
+
+        # TODO: vyřešit info o státu/kriminalitě
+        # TODO:
+        # přidat dodatečné  informace o směrodané odchylce
+        # přidat info o násobku z min -> max aj
+        # od jakého roku začal stát poskytovat/sbírat data - zmenit automaticky range ve widgete
+        # vytvorit slovnik jako zaznamnik pro porovnani s ostatnimi zememi průmery, max, min v ráci tr. činnu
+
+    def filter_data(self, country, crime):
+        self.country = country
+        self.crime = crime
+   
+
+        # TODO: nahradit kody '1-1' novymi tridami, ktere budou dedit od Statistics()
+        if country != 'all' and crime != 'all':
+            filtered_data = self.data[(self.data['country_name'] == country) & (
+                self.data['crime_info'] == crime)]
+            filtered_data = filtered_data.sort_values(
+                by='year', axis=0, ascending=True)
+            
+            # vznik noveho sloupce index!!
+            self.filtered_data = filtered_data.reset_index()
+            self.filtered_code = '1-1'
+            
+
+        elif country != 'all' and crime == 'all':
+            self.filtered_data = self.data[self.data['country_name'] == country]
+            self.filtered_code = '1-all'
+
+        elif country == 'all' and crime != 'all':
+            self.filtered_data = self.data[self.data['crime_info'] == crime]
+            self.filtered_code = 'all-1'
+
+        elif country == 'all' and crime == 'all':
+            self.filtered_data = self.data
+            self.filtered_code = 'all-all'
+
+        self._calculate_statistics()
+
+    def create_summary_df_1all(self):
+        dictionary_list = list()
+
+        for country_name in self.country_list_sorted[1:]:
+            for crime_name in self.crime_list_sorted[1:]:
+                country_crime_dict = {
+                    'country': country_name, 'crime': crime_name}
+                self.filter_data(country_name, crime_name)
+                self._calculate_statistics()
+                output_dictionary = self.statistics.statistics_dictionary
+                country_crime_dict.update(output_dictionary)
+                dictionary_list.append(country_crime_dict)
+        country_crime_info_11 = pd.DataFrame.from_dict(dictionary_list)
+
+        crime_categories = {
+            "Intentional homicide": "Visible",
+            "Attempted intentional homicide": "Visible",
+            "Serious assault": "Visible",
+            "Kidnapping": "Visible",
+            "Sexual violence": "Sensitive",
+            "Rape": "Sensitive",
+            "Sexual assault": "Sensitive",
+            "Sexual exploitation": "Sensitive",
+            "Child pornography": "Sensitive",
+            "Robbery": "Visible",
+            "Burglary": "Visible",
+            "Burglary of private residential premises": "Visible",
+            "Theft": "Visible",
+            "Theft of a motorized vehicle or parts thereof": "Visible",
+            "Unlawful acts involving controlled drugs or precursors": "Hidden",
+            "Fraud": "Hidden",
+            "Corruption": "Hidden",
+            "Bribery": "Hidden",
+            "Money laundering": "Hidden",
+            "Acts against computer systems": "Hidden",
+            "Participation in an organized criminal group": "Hidden"
+        }
+
+        country_crime_info_11["category_crime"] = country_crime_info_11["crime"].map(
+            crime_categories)
+        return country_crime_info_11
+
+    def __str__(self):
+        print(f'info {self.statistics}')
+
+    
